53fda55b4d33dddc478aaaae3fd08eff2ecf40fd
Administrator <Administrator@DESKTOP-95RB6IG>
2023-11-09 19:03:54 +0100

    Aggiungere i file di progetto.

---
+++ TheMatchChecksOut/Program.cs
@@ -0,0 +1,14 @@
+﻿int numero1;
+
+int numero2;
+
+Console.WriteLine("Inserisci il primo addendo");
+
+numero1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Inserisci il secondo addendo");
+
+numero2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Risultato " + (numero1 + numero2));
+
